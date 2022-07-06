dc77842ce0e03aa3630a6d8fe7ae0f43dda6e76d
Sehne, Julian <julian.sehne@student.fhws.de>
2022-07-06 09:42:38 +0200

    added TestMethods

--- src/test/java/de/fhws/fiw/fds/exam03/caching/GetStudyTripsTest.java
+++ src/test/java/de/fhws/fiw/fds/exam03/caching/GetStudyTripsTest.java
@@ -2,6 +2,7 @@ package de.fhws.fiw.fds.exam03.caching;
 
 import de.fhws.fiw.fds.exam02.client.rest.RestApiResponse;
 import de.fhws.fiw.fds.exam02.client.rest.resources.StudyTripRestClient;
+import de.fhws.fiw.fds.exam02.tests.models.Student;
 import de.fhws.fiw.fds.exam02.tests.models.StudyTrip;
 import de.fhws.fiw.fds.exam02.tests.util.headers.HeaderMap;
 import de.fhws.fiw.fds.exam02.tests.util.headers.HeaderMapUtils;
@@ -20,22 +21,83 @@ public class GetStudyTripsTest extends CachingTestHelper<StudyTrip, StudyTripRes
 			defineCacheUrl() + "studytrips");
 		assertTrue(response.headerExists("X-Proxy-Cache", "MISS"));
 		response = getCollectionRequestByUrl(HeaderMapUtils.withAcceptJson(), defineCacheUrl() + "studytrips");
-		assertTrue(response.headerExists("X-Proxy-Cache", "HIT"));
+		assertTrue(response.headerExists("X-Proxy-Cache", "MISS"));
 	}
 
-	@Test public void testGetSingleStudyTripCaching() throws IOException, InterruptedException
+	@Test public void testGetSingleStudyTripPrivateCachingMiss() throws IOException, InterruptedException
 	{
 		RestApiResponse<StudyTrip> response = getSingleRequestByUrl(HeaderMapUtils.withAcceptJson(),
 			defineCacheUrl() + "studytrips/2");
 		assertTrue(response.headerExists("X-Proxy-Cache", "MISS"));
-		response = getCollectionRequestByUrl(HeaderMapUtils.withAcceptJson(), defineCacheUrl() + "studytrips");
-		assertTrue(response.headerExists("X-Proxy-Cache", "HIT"));
+		response = getCollectionRequestByUrl(HeaderMapUtils.withAcceptJson(), defineCacheUrl() + "studytrips/2");
+		assertTrue(response.headerExists("X-Proxy-Cache", "MISS"));
 	}
 
-	//TODO single
-	//TODO Cache control existsxists
-	//TODO conditional get
-	//TODO conditional put
+	@Test public void test_cache_control_single() throws IOException
+	{
+		final RestApiResponse<StudyTrip> response = getSingleRequestById(HeaderMapUtils.withAcceptJson(), 2);
+		assertHeaderExists(response, CACHE_CONTROL);
+	}
+
+	@Test public void test_cache_control_collection() throws IOException
+	{
+		final RestApiResponse<StudyTrip> response = getCollectionRequest(HeaderMapUtils.withAcceptJson());
+		assertHeaderExists(response, CACHE_CONTROL);
+	}
+
+	@Test public void test_conditional_get_304() throws IOException
+	{
+		//user A loads a student
+		final RestApiResponse<StudyTrip> responseFromFirstGetRequest = getSingleRequestById(
+			HeaderMapUtils.withAcceptJson(), 1);
+
+		assertHeaderExists(responseFromFirstGetRequest, ETAG);
+
+		final String etag = responseFromFirstGetRequest.getEtagHeader();
+
+		//user A revalidates
+		final HeaderMap headersForSecondGetRequest = HeaderMapUtils.withAcceptJson();
+		headersForSecondGetRequest.addHeader(IF_NONE_MATCH, etag);
+
+		final RestApiResponse<StudyTrip> responseFromSecondGetRequest = getSingleRequestById(headersForSecondGetRequest,
+			1);
+
+		assertEquals(304, responseFromSecondGetRequest.getLastStatusCode());
+	}
+
+	@Test public void test_conditional_get_200() throws IOException
+	{
+		//user A loads a student
+		final RestApiResponse<StudyTrip> responseFromFirstGetRequest = getSingleRequestById(
+			HeaderMapUtils.withAcceptJson(), 1);
+		final StudyTrip student = responseFromFirstGetRequest.getResponseSingleData();
+
+		assertHeaderExists(responseFromFirstGetRequest, ETAG);
+
+		final String initialEtag = responseFromFirstGetRequest.getEtagHeader();
+
+		//user B updates this resource
+		student.setCityName("Munich");
+
+		final HeaderMap headersForPutRequestForUserB = HeaderMapUtils.withContentTypeJson();
+		headersForPutRequestForUserB.addHeader(IF_MATCH, initialEtag);
+
+		final RestApiResponse<StudyTrip> responseFromPutRequest = putRequest(headersForPutRequestForUserB, student);
+
+		assertEquals(204, responseFromPutRequest.getLastStatusCode());
+
+		final String newEtag = responseFromPutRequest.getEtagHeader();
+
+		//user A revalidates using old etag
+		final HeaderMap headersForSecondGetRequest = HeaderMapUtils.withAcceptJson();
+		headersForSecondGetRequest.addHeader(IF_NONE_MATCH, initialEtag);
+
+		final RestApiResponse<StudyTrip> responseFromSecondGetRequest = getSingleRequestById(headersForSecondGetRequest,
+			1);
+
+		assertEquals(200, responseFromSecondGetRequest.getLastStatusCode());
+		assertNotNull(responseFromSecondGetRequest.getResponseSingleData());
+	}
 
 	@Override protected StudyTripRestClient newRestClient(HeaderMap headers)
 	{
